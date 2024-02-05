dde6ea149b2e3f1e50209711cd1de2383ce5cbde
Bahodir <choriyevbahodir0>
2024-02-05 17:02:31 +0500

    something

--- main.js
+++ main.js
@@ -18,40 +18,63 @@ searchIput.addEventListener("input", ()=> {
     addData(filteredItems)
 })
 
+let determine;
+
 function addData (filteredItems) {
     itemList.innerHTML = ""
 
-    let itemsForList = filteredItems || thingsToDo
+    let itemsForList;
+
+    if (filteredItems) {
+        itemsForList = filteredItems
+        determine = "f"
+    } else {
+        itemsForList = thingsToDo
+        determine = "t"
+    }
 
     itemsForList.forEach(data => {
-    if (data.value) {
-
-        itemList.style.display = "block" 
-        const newItem = document.createElement("li") 
-        newItem.textContent = data.value 
-        newItem.className = "list-group-item" 
-        const newItem2 = document.createElement("div") 
-        newItem2.textContent = "x" 
-        newItem2.className = "addition" 
-     
-        itemList.append(newItem) 
-        newItem.append(newItem2) 
-        newItem2.addEventListener("click", function(event) { 
-            const li = event.target.parentElement
-            itemList.removeChild(li)
-            const indexToRemove = thingsToDo.findIndex(item => item.value === data.value);
-            thingsToDo.splice(indexToRemove, 1);
-            if (itemList.childElementCount == 0) {
-                itemList.style.display = "none"
-            }
-            updateLocalStorage()
-        }) 
-    }
+      
+        if (data.value) {
+            itemList.style.display = "block" 
+            const newItem = document.createElement("li") 
+            newItem.textContent = data.value 
+            newItem.className = "list-group-item" 
+            const newItem2 = document.createElement("div") 
+            newItem2.textContent = "x" 
+            newItem2.className = "addition" 
+         
+            itemList.append(newItem) 
+            newItem.append(newItem2) 
+            newItem2.addEventListener("click", function(event) { 
+                const li = event.target.parentElement
+                itemList.removeChild(li)
+                const indexToRemove = thingsToDo.findIndex(item => item.value === data.value);
+                thingsToDo.splice(indexToRemove, 1);
+                if (itemList.childElementCount == 0) {
+                    itemList.style.display = "none"
+                }
+                if (determine == "t" || filteredItems.length == thingsToDo.length) {
+                    updateDisplay()
+                }
+                updateLocalStorage()
+            }) 
+        }
     
     })
+    if (determine == "t" || filteredItems.length == thingsToDo.length) {
+        updateDisplay()
+    }
 
 }
 
+function updateDisplay () {
+    if (itemList.childElementCount == 0) {
+        searchIput.style.display = "none"
+    } else {
+        searchIput.style.display = "inline-block"
+    }
+}
     
 submitBtn.addEventListener("click", (event)=> {
     event.preventDefault()
